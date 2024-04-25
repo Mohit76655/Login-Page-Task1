3586809d753f2e90ddee75bee026aa83afd9ac3b
Mohit76655 <mohit76655@gmail.com>
2024-04-25 19:17:23 +0530

    CSS 80% Complete

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { toast } from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -28,23 +28,42 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={loginUser}>
-        <label>Email</label>
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <form
+        onSubmit={loginUser}
+        className="flex flex-col gap-5 border border-b-4 w-1/2 h-1/2 justify-center items-center rounded-md border-blue-100 bg-slate-200"
+      >
+        <h3 className=" font-bold text-xl">Login</h3>
+        {/* <label className="">Email</label> */}
         <input
+          className="border h-8 rounded-md p-4"
           type="email"
           placeholder="Enter Email..."
           value={data.email}
           onChange={(e) => setData({ ...data, email: e.target.value })}
         />
-        <label>Password</label>
+        {/* <label>Password</label> */}
         <input
-          type="text"
+          className="border h-8 rounded-md p-4"
+          type="password"
           placeholder="Enter Password..."
           value={data.password}
           onChange={(e) => setData({ ...data, password: e.target.value })}
         />
-        <button type="submit">Login</button>
+        <div className="flex gap-4">
+          <button
+            type="submit"
+            className="rounded-2xl p-1 border w-32 bg-blue-500 text-white hover:bg-blue-700"
+          >
+            Login
+          </button>
+          <Link
+            className="rounded-2xl p-1 border w-32 bg-blue-500 text-white text-center hover:bg-blue-700"
+            to="/register"
+          >
+            register
+          </Link>
+        </div>
       </form>
     </div>
   );
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -33,33 +33,47 @@ const Register = () => {
   };
 
   return (
-    <form onSubmit={registerUser}>
-      <label>Name</label>
-      <input
-        required
-        type="text"
-        placeholder="Enter Name..."
-        value={data.name}
-        onChange={(e) => setData({ ...data, name: e.target.value })}
-      />
-      <label>Email</label>
-      <input
-        required
-        type="email"
-        placeholder="Enter Email..."
-        value={data.email}
-        onChange={(e) => setData({ ...data, email: e.target.value })}
-      />
-      <label>Password</label>
-      <input
-        required
-        type="text"
-        placeholder="Enter Password..."
-        value={data.password}
-        onChange={(e) => setData({ ...data, password: e.target.value })}
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <form
+        className="flex flex-col gap-5 border border-b-4 w-1/2 h-1/2 justify-center items-center rounded-md border-blue-100 bg-slate-200"
+        onSubmit={registerUser}
+      >
+        <h3 className=" font-bold text-xl">Sign Up</h3>
+        {/* <label>Name</label> */}
+        <input
+          className="border h-8 rounded-md p-4"
+          required
+          type="text"
+          placeholder="Enter Name..."
+          value={data.name}
+          onChange={(e) => setData({ ...data, name: e.target.value })}
+        />
+        {/* <label>Email</label> */}
+        <input
+          className="border h-8 rounded-md p-4"
+          required
+          type="email"
+          placeholder="Enter Email..."
+          value={data.email}
+          onChange={(e) => setData({ ...data, email: e.target.value })}
+        />
+        {/* <label>Password</label> */}
+        <input
+          className="border h-8 rounded-md p-4"
+          required
+          type="text"
+          placeholder="Enter Password..."
+          value={data.password}
+          onChange={(e) => setData({ ...data, password: e.target.value })}
+        />
+        <button
+          className="rounded-2xl p-1 border w-32 bg-blue-500 text-white hover:bg-blue-700"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
   );
 };
 
